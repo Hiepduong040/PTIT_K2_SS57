665442bb0e4d4ff381344aeb6757b90dd2d3e1e3
unknown <you@example.com>
2024-06-10 12:30:26 +0700

    aadd

---
+++ src/components/Ex3/getProductById.tsx
@@ -0,0 +1,11 @@
+export default function GetProductById() {
+
+    let id=2;
+    fetch(`http://localhost:3000/products/${id}`)
+    .then(response=>response.json())
+    .then((data)=>{console.log(data);
+    })
+  return (
+    <div>GetProductById</div>
+  )
+}
