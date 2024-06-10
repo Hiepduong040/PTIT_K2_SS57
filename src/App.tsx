836f60250a51f24e6a0cfd1e9e814378a5ac997a
Hiepduong040 <147359314+Hiepduong040@users.noreply.github.com>
2024-06-10 13:16:25 +0700

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import CreateProduct from './components/CreateProduct'
+import GetAllProduct from './components/GetAllProduct'
+import GetProductById from './components/getProductById'
+import RemoveProductById from './components/RemoveProductById'
+import UpdateProductById from './components/UpdateProductById'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <GetAllProduct></GetAllProduct>
+      <GetProductById></GetProductById>
+      <RemoveProductById></RemoveProductById>
+      <CreateProduct></CreateProduct>
+      <UpdateProductById></UpdateProductById>
+      
+    </div>
   )
 }
-
-export default App
